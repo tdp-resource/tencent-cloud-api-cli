8b479a20494306fc14d09a0d3845123a9828a24a
若海 <wang@rehiy.com>
2022-07-29 11:08:59 +0800

    增加linux编译脚本

---
+++ build.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+#
+
+export CGO_ENABLED=0
+
+build() {
+    GOOS=$1
+    GOARCH=$2
+    echo building for $1/$2
+    go build -o build/tcapi-$1-$2$3 main.go
+}
+
+####################################################################
+
+build android arm64
+
+build darwin amd64
+build darwin arm64
+
+build linux 386
+build linux amd64
+build linux arm64
+
+build windows 386 .exe
+build windows amd64 .exe
+build windows arm64 .exe
